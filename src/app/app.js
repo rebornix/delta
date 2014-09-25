3e1ed0060a009f80904cf265c91b77b408136809
penlv <penlv@microsoft.com>
2014-09-25 22:45:37 +0800

    enable cors

--- src/app/app.js
+++ src/app/app.js
@@ -6,8 +6,8 @@
         delete $httpProvider.defaults.headers.common['X-Requested-With'];
         $httpProvider.defaults.headers.common["Content-Type"] = "application/json";
         $httpProvider.defaults.headers.common["Accept"] = "application/json";
-    }
-    ]);
+        $httpProvider.defaults.withCredentials = true;
+    }]);
     app.config(function ($stateProvider, $urlRouterProvider) {
         $stateProvider
             .state('index', {
